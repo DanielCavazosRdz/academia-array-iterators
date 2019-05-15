311a23d6b168ed2e06d89037d5faf4b351e4e88c
Daniel Cavazos <soopycorn@gmail.com>
2019-05-15 11:48:11 -0700

    workHere.js completed

--- workHere.js
+++ workHere.js
@@ -4,7 +4,10 @@ var billionaires = require('./billionaires.json');
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+let twentyYearOldBillionaires =  billionaires.filter(element => {
+    return element.demographics.age >= 20 && element.demographics.age <= 29;
+});
+
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,32 +18,61 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
+let roundedWorth = billionaires.map(element => {
+    return {
+        year: element.year,
+        name: element.name,
+        worth: Math.round(element.wealth["worth in billions"])
+    };
+});
 
 /*
     sum the rounded worth of all the billionaires
 */
 
-let aggregatedWorth = null;
-// console.log(aggregatedWorth)
+let aggregatedWorth = billionaires.reduce((prev, element) => {
+    return prev + (Math.round(element.wealth["worth in billions"]));
+} ,0);
+//console.log(aggregatedWorth());
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
+let richest = [billionaires.reduce((prev, element) => {
+    if(Object.keys(prev).length === Object.keys({}).length){
+        prev = element;
+        return prev;
+    }
+    if(prev.rank > element.rank){
+        prev = element;
+        return prev;
+    }
+    return prev;
+} , {})];
+ console.log(richest);
 
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+let poorest = [billionaires.reduce((prev, element) => {
+    if(Object.keys(prev).length === Object.keys({}).length){
+        prev = element;
+        return prev;
+    }
+    if(prev.rank <= element.rank){
+        prev = element;
+        return prev;
+    }
+    return prev;
+}, {})];
 
 /*
     make a new array containing the richest and the poorest billionaire
 */
 
-let richAndPoor = null;
+let richAndPoor = [richest, poorest];
 
 module.exports = {
     twentyYearOldBillionaires,
